661bd217e20214a3551d5b041fc9b5ce0b1f210d
Lucile THIEULIN <lucile.thieulin96@gmail.com>
2024-01-14 17:55:24 +0100

    suppression fichiers inutiles, mise en place de la police, mise en place varibale couleur

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
+import "../style.css"
  
 function App({ Component, pageProps }) {
     return (
         <>
         <Head>
         <title>Multi-step-form</title>
+        
       </Head>
      
         <Component {...pageProps} />
